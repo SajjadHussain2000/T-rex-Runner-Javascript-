6b0cc1516b4207a9c90303c9a9604522715d52a4
SajjadHussain2000 <sh267002@gmail.com>
2021-10-17 20:30:28 +0530

    adding existing files

---
+++ script.js
@@ -0,0 +1,29 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function(){
+
+    let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if(cactusleft<50 && cactusleft>0 && dinotop>=140)
+    {
+        //console.log("collosion");
+        alert("Game Over"); 
+    }
+
+},10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
